1abd7bd16ad3a0cba7a03dcf60dbb7c4ccfa63f7
Abeeb Ridwan <olumideuae@gmail.com>
2019-03-15 22:23:36 +0400

    material Ui integration and app 1-start completed

--- book/1-start/pages/index.js
+++ book/1-start/pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Button from '@material-ui/core/Button';
+import withLayout from '../lib/withLayout';
 
 const Index = () => (
   <div style={{ padding: '10px 45px' }}>
@@ -7,7 +9,8 @@ const Index = () => (
       <meta name="description" content="This is the description of the Index page" />
     </Head>
     <p>Content on Index page</p>
+    <Button variant="contained">MUI button</Button>
   </div>
 );
 
-export default Index;
+export default withLayout(Index);
